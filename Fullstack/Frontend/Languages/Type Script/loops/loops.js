0237de3f83b7a36eccccd2a542dc35ecfd483eed
ashapamarthi997 <asha.pamarthi@toucanus.com>
2023-06-04 18:40:59 +0530

    operators

--- Fullstack/Frontend/Languages/Type Script/loops/loops.js
+++ Fullstack/Frontend/Languages/Type Script/loops/loops.js
@@ -1,11 +1,12 @@
 // for(var i=0;i<=10;i++){
 //     console.log(i)
 // }
-var j=21
+var j=10
 // while(j<=20){
 //     console.log(`thid id while ${j}`)
 //     j++
 // }
 do{
     console.log("do while ")
+    j++
 }while(j<20)
